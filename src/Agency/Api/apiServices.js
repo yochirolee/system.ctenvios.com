156e636ac3a5c4cc14a5cfbc248134ca20b54987
yochirolee <yleecruz@gmail.com>
2023-08-11 10:18:03 -0400

    api production url added

--- src/Agency/Api/apiServices.js
+++ src/Agency/Api/apiServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const development_URL = "http://localhost:3001/api/v1";
 const production_URL = "https://api-ctenvios.vercel.app/api/v1";
-axios.defaults.baseURL = development_URL;
+axios.defaults.baseURL = production_URL;
 
 const apiServices = {
 	agencies: {
